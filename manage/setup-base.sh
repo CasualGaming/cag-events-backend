5affd75decfa39fc3d05f45fe4a5d62288d70552
Vestein Dahl <vesteind@stud.ntnu.no>
2019-03-14 13:35:26 +0100

    Adds check for osx when determining python path.

--- manage/setup-base.sh
+++ manage/setup-base.sh
@@ -2,15 +2,22 @@
 
 set -eu # Exit on error and undefined var is error
 
+PYTHON_PATH=
+
+case "$OSTYPE" in
+  darwin*)  PYTHON_PATH=$(which python3) ;; 
+  *)        PYTHON_PATH=$(which python) ;;
+esac
+
 # Setup virtual environment to install packages and stuff inside
 if [[ ! -d .venv ]]; then
     echo "Creating venv ..."
     # Don't use symlinks if in VirtualBox shared folder
     if ( df -t vboxsf . 1>/dev/null 2>/dev/null ); then
         echo "VirtualBox shared folder detected"
-        virtualenv -p $(which python) --always-copy .venv
+        virtualenv -p $PYTHON_PATH --always-copy .venv
     else
-        virtualenv -p $(which python) .venv
+        virtualenv -p $PYTHON_PATH .venv
     fi
 fi
 
