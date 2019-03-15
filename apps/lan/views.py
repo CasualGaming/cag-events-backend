31056cf5093478921e0daf7f26edf9a2a30de58f
Vestein Dahl <vesteindahl@hotmail.com>
2019-03-15 19:52:33 +0100

    Adds /lan/:id/news.

--- apps/lan/views.py
+++ apps/lan/views.py
@@ -1,4 +1,8 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from apps.news.serializers import ArticleSerializer
 
 from .models import LAN
 from .serializers import LANSerializer
@@ -8,3 +12,9 @@ class LANViewSet(viewsets.ModelViewSet):
     queryset = LAN.objects.all()
     serializer_class = LANSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+
+    @action(detail=True)
+    def news(self, request, *args, **kwargs):
+        queryset = self.get_object().article_set.all()
+        serializer = ArticleSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
