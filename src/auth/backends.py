746e9be68d192f51058c4270544931210eb2bacc
Håvard O. Nordstrand <haavard@haavard.tech>
2019-06-18 18:14:26 +0200

    Improve OIDC backend (didn't have time to test)

--- src/auth/backends.py
+++ src/auth/backends.py
@@ -12,6 +12,8 @@ from apps.user.models import GroupExtension, User, UserProfile
 class OidcAuthBackend(OIDCAuthenticationBackend):
 
     username_regex = re.compile(r"^[a-z0-9_]+$")
+    username_min_length = 3
+    username_max_length = 25
     membership_years_regex = re.compile(r"^([0-9]{4},)*([0-9]{4})?$")
 
     def filter_users_by_claims(self, claims):
@@ -22,18 +24,22 @@ class OidcAuthBackend(OIDCAuthenticationBackend):
             return User.objects.none()
 
     def create_user(self, claims):
-        attributes = self.get_user_attributes(claims)
-        self.validate_user_attributes(attributes)
+        # Check if provided user attributes are okay first
+        attributes = self.decode_attributes(claims)
+        self.validate_attributes(attributes)
+
         user = User.objects.create(subject_id=attributes["subject_id"],
                                    username=attributes["username"],
                                    email=attributes["email"])
         UserProfile.objects.create(user=user)
+
         return self.update_user(user, claims, attributes)
 
     def update_user(self, user, claims, attributes=None):
+        # Check if provided user attributes are okay first, if not already checked
         if attributes is None:
-            attributes = self.get_user_attributes(claims)
-            self.validate_user_attributes(attributes)
+            attributes = self.decode_attributes(claims)
+            self.validate_attributes(attributes)
 
         # Update user and userprofile
         user.username = attributes["username"]
@@ -77,7 +83,7 @@ class OidcAuthBackend(OIDCAuthenticationBackend):
         return user
 
     @classmethod
-    def get_user_attributes(cls, claims):
+    def decode_attributes(cls, claims):
         attributes = {}
 
         attributes["subject_id"] = cls.get_claim(claims, "sub")
@@ -90,36 +96,37 @@ class OidcAuthBackend(OIDCAuthenticationBackend):
         attributes["gender"] = cls.get_claim(claims, "gender")
         attributes["phone_number"] = cls.get_claim(claims, "phone_number")
 
-        address_claims = cls.get_claim(claims, "address")
-        if not isinstance(address_claims, dict):
-            raise SuspiciousOperation("Attribute 'address' is not a dict")
+        address_claims = cls.get_claim(claims, "address", expected_type=dict)
         attributes["country"] = cls.get_claim(address_claims, "country")
         attributes["postal_code"] = cls.get_claim(address_claims, "postal_code")
         attributes["street_address"] = cls.get_claim(address_claims, "street_address")
 
-        (membership_years, is_member) = cls.get_membership_years(claims, "membership_years")
+        (membership_years, is_member) = cls.decode_membership_years(claims, "membership_years")
         attributes["membership_years"] = membership_years
         attributes["is_member"] = is_member
 
-        groups = cls.get_claim(claims, "groups")
-        if not isinstance(groups, list):
-            raise SuspiciousOperation("Attribute 'groups' is not a list")
+        groups = cls.get_claim(claims, "groups", expected_type=list)
         attributes["groups"] = groups
 
         return attributes
 
     @classmethod
-    def validate_user_attributes(cls, attributes):
+    def validate_attributes(cls, attributes):
+        # Verify username and pretty_username
         if not cls.username_regex.match(attributes["username"]):
-            raise SuspiciousOperation("Invalid username")
+            raise SuspiciousOperation("Invalid username format")
+        if len(attributes["username"]) < cls.username_min_length:
+            raise SuspiciousOperation("Username too short")
+        if len(attributes["username"]) > cls.username_max_length:
+            raise SuspiciousOperation("Username too long")
         if attributes["pretty_username"].lower() != attributes["username"]:
-            raise SuspiciousOperation("Invalid pretty username")
+            raise SuspiciousOperation("Pretty username doesn't match username")
 
     @classmethod
-    def get_membership_years(cls, claims, key, year=datetime.today().strftime("%Y")):
-        membership_years = cls.get_claim(claims, key)
+    def decode_membership_years(cls, claims, key, year=datetime.today().strftime("%Y")):
+        membership_years = cls.get_claim(claims, key, allow_empty=True)
         if not cls.membership_years_regex.match(membership_years):
-            raise SuspiciousOperation("Invalid membership years: " + membership_years)
+            raise SuspiciousOperation("Invalid membership years format: " + membership_years)
         is_member = False
         if len(membership_years) > 0:
             fragments = membership_years.split(",")
@@ -127,8 +134,12 @@ class OidcAuthBackend(OIDCAuthenticationBackend):
         return membership_years, is_member
 
     @staticmethod
-    def get_claim(claims, key):
+    def get_claim(claims, key, allow_empty=False, expected_type=str):
         value = claims.get(key)
         if value is None:
-            raise SuspiciousOperation("Missing claim: " + key)
+            raise SuspiciousOperation("Claim '" + key + "' is missing")
+        if value == "" and not allow_empty:
+            raise SuspiciousOperation("Claim '" + key + "' is empty")
+        if value != "" and not isinstance(value, expected_type):
+            raise SuspiciousOperation("Claim '" + key + "' is not a " + str(expected_type) + " but a " + str(type(value)))
         return value
