6361ffca1348a27c6ae3be9623b8c82a65eadccd
Håvard O. Nordstrand <haavard@haavard.tech>
2019-06-28 19:22:51 +0200

    Update seating SVG generator

--- src/apps/seating/generators.py
+++ src/apps/seating/generators.py
@@ -1,14 +1,14 @@
 from bs4 import BeautifulSoup
 
 
+SEAT_BORDER_WIDTH = 0.05
+
+
 def generate_area_layout(area_layout):
     doc = BeautifulSoup("<svg>", "html.parser")
     svg_element = doc.svg
     svg_element["viewBox"] = "0 0 {0} {1}".format(area_layout.width, area_layout.height)
     svg_element["xmlns"] = "http://www.w3.org/2000/svg"
-
-    # Debug background
-    # svg_element.append(doc.new_tag("rect", width=area_layout.width, height=area_layout.height, fill="red"))
 
     for row_layout in area_layout.row_layouts.all():
         row_rotation = "translate({0} {1})".format(row_layout.offset_horizontal, row_layout.offset_vertical)
@@ -28,15 +28,15 @@ def generate_area_layout(area_layout):
 
 def _generate_area_layout_seat(doc, row_layout, seat_num_vertical, seat_num_horizontal):
     seat_num = (seat_num_vertical - 1) * row_layout.seat_count_horizontal + seat_num_horizontal
-    seat_id = "seat-{0}-{1}".format(row_layout.row_number, seat_num)
     seat_element = doc.new_tag("a")
-    seat_element["id"] = seat_id
     seat_element["class"] = "seat"
-    seat_element["href"] = "#{0}".format(seat_id)
+    seat_element["href"] = "#"
+    seat_element["data-row-num"] = row_layout.row_number
+    seat_element["data-seat-num"] = seat_num
     content_element = doc.new_tag("rect")
-    content_element["width"] = row_layout.seat_width
-    content_element["height"] = row_layout.seat_height
-    content_element["x"] = (seat_num_horizontal - 1) * (row_layout.seat_width + row_layout.seat_spacing_horizontal)
-    content_element["y"] = (seat_num_vertical - 1) * (row_layout.seat_height + row_layout.seat_spacing_vertical)
+    content_element["width"] = row_layout.seat_width - 2 * SEAT_BORDER_WIDTH
+    content_element["height"] = row_layout.seat_height - 2 * SEAT_BORDER_WIDTH
+    content_element["x"] = (seat_num_horizontal - 1) * (row_layout.seat_width + row_layout.seat_spacing_horizontal) + SEAT_BORDER_WIDTH
+    content_element["y"] = (seat_num_vertical - 1) * (row_layout.seat_height + row_layout.seat_spacing_vertical) + SEAT_BORDER_WIDTH
     seat_element.append(content_element)
     return seat_element
