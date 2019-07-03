084a88deb740930fe435a5a750d620d2d29fa20f
Håvard O. Nordstrand <haavard@haavard.tech>
2019-07-03 05:22:18 +0200

    Fix Travis CI

--- manage/lint.sh
+++ manage/lint.sh
@@ -3,9 +3,10 @@
 # Runs linter.
 
 CMD="manage/cmd.sh"
+SRC_DIR="src"
 
 set -eu # Exit on error and undefined var is error
 
 # Run flake8 static code analysis
 # Uses settings from .flake8
-$CMD flake8
+$CMD flake8 $SRC_DIR
